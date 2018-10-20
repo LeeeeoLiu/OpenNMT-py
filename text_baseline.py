37b20db13ff7c70f94464060fc7a358ff0634ce6
Leo Liu <1010820319@qq.com>
2018-10-20 19:01:57 +0800

    fix bug in text baseline

--- text_baseline.py
+++ text_baseline.py
@@ -12,6 +12,7 @@ from sumy.utils import get_stop_words
 
 import argparse
 from rouge import Rouge
+import os
 
 def LexRank_Text(originalText, LANGUAGE="chinese"):
     """Get LexRank output from a text.
@@ -25,11 +26,10 @@ def LexRank_Text(originalText, LANGUAGE="chinese"):
     Returns:
         str
     """
-    #print(originalText)
     parser = PlaintextParser.from_string(originalText, Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
     summarizer = LexRank(stemmer)
-    # summarizer.stop_words = get_stop_words(LANGUAGE)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
     # print(summarizer(parser.document, 1))
     for sentence in summarizer(parser.document, 1):
         return str(sentence)
@@ -41,43 +41,74 @@ def read_and_filter(_file_path):
         for line in f:
             line = line.decode('utf-8')
             sessions = line.strip('\n').split('||')
-            #if len(sessions) < 20:
             for s in sessions:
                 assert len(s.split('\t')) == 11
             item_name = [s.split('\t')[9].split() for s in sessions]
             item_comment = [s.split('\t')[10].split() for s in sessions]
             _new_line = []
+            _session_length = []
             for tmp_name, tmp_comment in zip(item_name, item_comment):
                 _new_line.extend(tmp_name)
                 _new_line.extend(tmp_comment)
             _new_lines.append(_new_line)
-        return _new_lines
+            _session_length.append(len(tmp_name))
+        return _new_lines, _session_length
         
+
+def write_file(_lines, _path):
+    _lines = [_line.encode('utf-8') for _line in _lines]
+    with open(_path, 'w') as f:
+        f.write('\n'.join(_lines))
+
+def read_file(_path):
+    with open(_path, 'r') as f:
+        lines = [line.strip('\n') for line in f]
+        return lines
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Parameters description")
-    parser.add_argument('-src', type=str, help="Source File Path")
-    parser.add_argument('-tgt', type=str, help="Target File Path")
+    parser.add_argument('-src', type=str, help="Source file path.")
+    parser.add_argument('-tgt', type=str, help="Target file path.")
     parser.add_argument('-baseline', type=str,
-                        help="Baseline Method for Explanation Generation")
+                        help="Baseline Method for Explanation Generation.")
+    parser.add_argument('-save', type=str, help="Path to save the output file.")
+    parser.add_argument('-test', type=int,default=-1, help='Test the code with a small number of examples.')
     args = parser.parse_args()
-    
+    LANGUAGE = "chinese"
+
     assert args.baseline in ['lexrank']
     if args.baseline == 'lexrank':
-        _src_lines = read_and_filter(args.src)
-        _tgt_lines = read_and_filter(args.tgt)
-        assert len(_src_lines) == len(_tgt_lines)
-        LANGUAGE="chinese"
+        if os.path.exists(args.save+'/lexrank_src.txt'):
+            src_lines = read_file(args.save+'/lexrank_src.txt')
+            tgt_lines = read_file(args.save+'/lexrank_tgt.txt')
+        else:
+            _src_lines, _lengths = read_and_filter(args.src)
+            _tgt_lines, _ = read_and_filter(args.tgt)
+            src_lines = []
+            tgt_lines = []
+            assert len(_src_lines) == len(_tgt_lines), len(
+                _src_lines) == len(_lengths)
+            for src_line, tgt_line, length in zip(_src_lines, _tgt_lines, _lengths):
+                if length < 20:
+                    src_lines.append(' '.join(src_line))
+                    tgt_lines.append(' '.join(tgt_line))
+            write_file(src_lines, args.save+'/lexrank_src.txt')
+            write_file(tgt_lines, args.save+'/lexrank_tgt.txt')
+        
         _sum_lines = []
-        i = 0
-        for _line in _src_lines:
-            print('process: {}/{}'.format(i,len(_src_lines)))
+        if args.test > 0:
+            src_lines = src_lines[0:args.test]
+            tgt_lines = tgt_lines[0:args.test]
+        for i, _line in enumerate(src_lines):
+            print('process: {}/{}'.format(i,len(src_lines)))
             _sum_line = LexRank_Text(_line, LANGUAGE) if LexRank_Text(_line, LANGUAGE) != None else ''
             _sum_lines.append(_sum_line)
-            i += 1
-        #_sum_lines = [LexRank_Text(_line, LANGUAGE) if LexRank_Text(_line, LANGUAGE) != None else '' for _line in _src_lines]
+
+        assert len(_sum_lines) == len(tgt_lines)
         rouge = Rouge()
-        scores = rouge.get_scores(_sum_lines, _tgt_lines, avg=True)
+        write_file(_sum_lines, args.save+'/lexrank_sumy.txt')
+        scores = rouge.get_scores(_sum_lines, tgt_lines, avg=True)
         
         _sentence_msg = 'ROUGE-1: P:{}\tR:{}\tF1:{}'.format(
             scores['rouge-1']['p'], scores['rouge-1']['r'], scores['rouge-1']['f'])
@@ -86,3 +117,6 @@ if __name__ == '__main__':
         _sentence_msg += '\nROUGE-L: P:{}\tR:{}\tF1:{}'.format(
             scores['rouge-l']['p'], scores['rouge-l']['r'], scores['rouge-l']['f'])
         print(_sentence_msg)
+        
+
+        
